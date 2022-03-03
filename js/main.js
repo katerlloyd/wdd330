a37afd784beedb04348f0110e3ed42862527edfa
katereclark <cla16022@byui.edu>
2022-03-03 09:58:59 -0700

    Added week 9 notes

--- js/main.js
+++ js/main.js
@@ -30,6 +30,22 @@ const array = [
     {
         label: 'Week 8 Notes', 
         link: 'week8/index.html'
+    },
+    {
+        label: 'Week 9 Notes', 
+        link: 'week9/index.html'
+    },
+    {
+        label: 'Week 10 Notes', 
+        link: 'week10/index.html'
+    },
+    {
+        label: 'Week 11 Notes', 
+        link: 'week11/index.html'
+    },
+    {
+        label: 'Week 12 Notes', 
+        link: 'week12/index.html'
     }];
 
 let ol = document.querySelector('ol');
